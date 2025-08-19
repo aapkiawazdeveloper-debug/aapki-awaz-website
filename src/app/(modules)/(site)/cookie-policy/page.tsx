5bfc26d38866ded91a736d6d363d9efa3e296d2f
aapkiawazdeveloper-debug <aapkiawazdeveloper@gmail.com>
2025-08-19 15:47:23 +0530

    footer update

---
+++ src/app/(modules)/(site)/cookie-policy/page.tsx
@@ -0,0 +1,12 @@
+const CookiePolicyPage = () => {
+  return (
+    <>
+      <div>
+        <h1>test</h1>
+      </div>
+      <div></div>
+    </>
+  );
+};
+
+export default CookiePolicyPage;
--- src/app/(modules)/(site)/layout.tsx
+++ src/app/(modules)/(site)/layout.tsx
@@ -7,39 +7,40 @@ import Footer from "@/app/shared/components/site/layout/Footer";
 import Sidebar from "@/app/shared/components/site/layout/Sidebar";
 
 const inter = Inter({
-    subsets: ["latin"],
-    variable: "--font-inter",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
 });
 
 const poppins = Poppins({
-    subsets: ["latin"],
-    weight: ["400", "500", "600", "700"],
-    variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
 });
 
 export const metadata: Metadata = {
-    title: "Next.js site",
-    description: "Next.js site",
+  title: "Next.js site",
+  description: "Next.js site",
 };
 
 const SiteLayout = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <html lang="en" className={`${inter.variable} ${poppins.variable}`}>
-            <body className="font-inter">
-                <Header />
+  return (
+    <html lang="en" className={`${inter.variable} ${poppins.variable}`}>
+      <body className="font-inter">
+        <Header />
 
-                <div className="container mx-auto px-4 py-4 grid grid-cols-12 gap-6">
-                    <main className="col-span-12 lg:col-span-9">{children}</main>
+        <div className="container mx-auto px-4 py-4 grid grid-cols-12 gap-6">
+          <main className="col-span-12 lg:col-span-9">{children}</main>
 
-                    <div className="col-span-12 lg:col-span-3">
-                        <Sidebar />
-                    </div>
-                </div>
+          <div className="col-span-12 lg:col-span-3">
+            <Sidebar />
+          </div>
+        </div>
 
-                <Footer />
-            </body>
-        </html>
-    );
+        <Footer />
+      </body>
+    </html>
+  );
 };
 
 export default SiteLayout;
