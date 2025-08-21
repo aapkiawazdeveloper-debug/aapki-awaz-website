33baf58f92e9fa44208afcba30298fd4812cb8a6
aapkiawazdeveloper-debug <aapkiawazdeveloper@gmail.com>
2025-08-21 10:38:59 +0530

    privacy policy page design

---
+++ src/app/(modules)/(site)/privacy-policy/page.tsx
@@ -0,0 +1,13 @@
+import PrivacyPolicy from "@/app/shared/components/site/PrivacyPolicy";
+import PageHeader from "@/app/shared/components/site/ui/PageHeader";
+
+const PrivacyPolicyPage = () => {
+  return (
+    <>
+      <PageHeader pageTitle="Privacy Policy" />
+      <PrivacyPolicy />
+    </>
+  );
+};
+
+export default PrivacyPolicyPage;
