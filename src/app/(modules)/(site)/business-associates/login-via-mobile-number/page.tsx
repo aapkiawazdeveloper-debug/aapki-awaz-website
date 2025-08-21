ba657f76fbda214a312f8e5bb889c17b824a3e7d
aapkiawazdeveloper-debug <aapkiawazdeveloper@gmail.com>
2025-08-21 17:35:11 +0530

    reporter and business associates page setup

---
+++ src/app/(modules)/(site)/business-associates/login-via-mobile-number/page.tsx
@@ -0,0 +1,9 @@
+const LoginViaMobileNumberPage = () => {
+  return (
+    <div>
+      <h1>LoginViaMobileNumberPage</h1>
+    </div>
+  );
+};
+
+export default LoginViaMobileNumberPage;
---
+++ src/app/(modules)/(site)/business-associates/login-via-userid/page.tsx
@@ -0,0 +1,9 @@
+const LoginViaUseridPage = () => {
+  return (
+    <div>
+      <h1>LoginViaUseridPage</h1>
+    </div>
+  );
+};
+
+export default LoginViaUseridPage;
