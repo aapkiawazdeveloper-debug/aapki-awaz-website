ff3d84775dad4d4719cc99da8e8407ed578e6a29
aapkiawazdeveloper-debug <aapkiawazdeveloper@gmail.com>
2025-08-21 14:46:39 +0530

    leader of rajasthan minister mla mp district page design

---
+++ src/app/(modules)/(site)/leader-of-rajasthan-minister-mla-mp-district/page.tsx
@@ -0,0 +1,33 @@
+import Pagination from "@/app/shared/components/site/Pagination";
+import PoliticianCard from "@/app/shared/components/site/ui/cards/PoliticianCard";
+import PoliticianFilter from "@/app/shared/components/site/ui/filters/PoliticianFilter";
+import PageHeader from "@/app/shared/components/site/ui/PageHeader";
+
+const buttons = [
+  {
+    id: 1,
+    title: "View All",
+    link: "/",
+  },
+];
+
+const LeaderOfRajasthanMinisterMlaMpDistrictPage = () => {
+  return (
+    <>
+      <PageHeader pageTitle="Mayor - Chairman - President" buttons={buttons} />
+
+      <PoliticianFilter />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {Array(13)
+          .fill(null)
+          .map((_, index) => (
+            <PoliticianCard key={index} />
+          ))}
+      </div>
+
+      <Pagination />
+    </>
+  );
+};
+
+export default LeaderOfRajasthanMinisterMlaMpDistrictPage;
