50e23dc65617eac7e46b973f020464fda29e1410
aapkiawazdeveloper-debug <aapkiawazdeveloper@gmail.com>
2025-08-23 16:54:18 +0530

    astrology page deisng

---
+++ src/app/(modules)/(site)/leader-of-rajasthan/page.tsx
@@ -0,0 +1,11 @@
+import PageHeader from "@/app/shared/components/site/ui/PageHeader";
+
+const LeaderOfRajasthanPage = () => {
+  return (
+    <div>
+      <PageHeader pageTitle="Leader of Rajasthan" />
+    </div>
+  );
+};
+
+export default LeaderOfRajasthanPage;
--- src/app/(modules)/(site)/religion/[id]/page.tsx
+++ src/app/(modules)/(site)/religion/[id]/page.tsx
@@ -1,8 +1,25 @@
+import Pagination from "@/app/shared/components/site/Pagination";
+import ArticleCard from "@/app/shared/components/site/ui/cards/ArticleCard";
+import PageFilter from "@/app/shared/components/site/ui/filters/PageFilter";
+import PageHeader from "@/app/shared/components/site/ui/PageHeader";
+
 const ReligionDetailsPage = () => {
   return (
-    <div>
-      <h1>ReligionDetailsPage</h1>
-    </div>
+    <>
+      <PageHeader pageTitle="Astrology" />
+
+      <PageFilter />
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {Array(13)
+          .fill(null)
+          .map((_, index) => (
+            <ArticleCard key={index} />
+          ))}
+      </div>
+
+      <Pagination />
+    </>
   );
 };
 
