cbd1cc787f60f2a251e6451cbc2be9afa7d80b46
aapkiawazdeveloper-debug <aapkiawazdeveloper@gmail.com>
2025-09-03 16:39:31 +0530

    pagination added on the news api

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -1,10 +1,19 @@
-import { getNews } from "./controller";
+import { getNews, getNewsByPagination } from "./controller";
 
 /**
- * GET: Retrieve all categories
- * Endpoint: /api/categories
+ * GET: Retrieve news (all or paginated)
+ * Endpoint: /api/news?page=1&limit=20
  */
+export const GET = async (req: Request) => {
+  const url = new URL(req.url);
+  const page = url.searchParams.get("page");
+  const limit = url.searchParams.get("limit");
 
-export const GET = async () => {
-  return getNews();
+  if (page && limit) {
+    const pageNumber = parseInt(page, 10);
+    const limitNumber = parseInt(limit, 10);
+    return getNewsByPagination(pageNumber, limitNumber);
+  } else {
+    return getNews();
+  }
 };
