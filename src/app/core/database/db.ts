157b5939f81cc4dc483a171951adf8f2e43fe7ac
aapkiawazdeveloper-debug <aapkiawazdeveloper@gmail.com>
2025-09-03 18:32:31 +0530

    database updat

--- src/app/core/database/db.ts
+++ src/app/core/database/db.ts
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "Rohit1234",
-  database: "newschema",
+  database: "akap_aapkiawa_next",
 });
 
 export default pool;
