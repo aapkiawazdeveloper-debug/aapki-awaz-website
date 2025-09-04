bd450424ceb5448f43ce43f0f49a172ee5e1c32b
aapkiawazdeveloper-debug <aapkiawazdeveloper@gmail.com>
2025-09-04 17:55:55 +0530

    search button add icon and design changes in the page filter

--- src/app/shared/components/site/ui/filters/PageFilter.tsx
+++ src/app/shared/components/site/ui/filters/PageFilter.tsx
@@ -1,12 +1,18 @@
-import Button from "../buttons/Button";
 import CustomInput from "../form/CustomInput";
+import { FiSearch } from "react-icons/fi";
 
 const PageFilter = () => {
   return (
     <div className="mb-4">
       <form className="flex items-center gap-4">
         <CustomInput />
-        <Button />
+        <button
+          type="submit"
+          className="flex items-center gap-2.5 px-6 py-1.5 text-center text-sm font-medium rounded-sm bg-blue-600 text-white hover:bg-blue-700 transition cursor-pointer"
+        >
+          <FiSearch size={16} />
+          <span>Search</span>
+        </button>
       </form>
     </div>
   );
